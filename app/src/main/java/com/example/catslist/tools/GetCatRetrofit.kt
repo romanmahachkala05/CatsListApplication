e93bdcadafa030f7eb0fb9f4b8e38633f6d212ed
Roman <romansakhwork@gmail.com>
2022-12-15 15:36:02 +0300

    Created retrofit request to get cats

---
+++ app/src/main/java/com/example/catslist/tools/GetCatRetrofit.kt
@@ -0,0 +1,15 @@
+package com.example.catslist.tools
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object GetCatRetrofit {
+
+    const val baseUrl = "https://api.thecatapi.com"
+    val retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    fun getService() = retrofit.create(CatRequest::class.java)
+}
