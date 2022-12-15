3e01609a7317cb0cfa3d29a03fbc427f630be82c
Roman <romansakhwork@gmail.com>
2022-12-15 21:53:50 +0300

    Created CatStorage object to place there logic of getting new cats

---
+++ app/src/main/java/com/example/catslist/tools/CatStorage.kt
@@ -0,0 +1,13 @@
+package com.example.catslist.tools
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import com.example.catslist.models.Cat
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+
+object CatStorage {
+
+}
