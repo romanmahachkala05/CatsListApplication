1cec39888cfe3b339e54f0ec164f4c13975d2efc
Roman <romansakhwork@gmail.com>
2022-12-15 17:20:17 +0300

    WIP CatsAdapter.kt: added catsList and CatsViewHolder, onCreateViewHolder, fixed typo

--- app/src/main/java/com/example/catslist/viewmodels/CatsAdapter.kt
+++ app/src/main/java/com/example/catslist/viewmodels/CatsAdapter.kt
@@ -1,24 +1,32 @@
 package com.example.catslist.viewmodels
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.catslist.databinding.ItemCatBinding
+import com.example.catslist.models.Cat
 
-class CatsAdapter : RecyclerView.Adapter<CatsAdapter.CatsViewBinding> {
+class CatsAdapter : RecyclerView.Adapter<CatsAdapter.CatsViewHolder>() {
 
-    class CatsViewBinding(
+    var catsList: List<Cat> = emptyList()
+        set(newValue) {
+            field = newValue
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatsViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val binding = ItemCatBinding.inflate(inflater, parent,false)
+        return CatsViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: CatsViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = catsList.size
+
+    class CatsViewHolder(
         val binding: ItemCatBinding
     ) : RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CatsViewBinding {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(holder: CatsViewBinding, position: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
 }
