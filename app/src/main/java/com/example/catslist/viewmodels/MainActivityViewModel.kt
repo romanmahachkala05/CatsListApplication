8cd0663c061920d9b7f327400525b62ea7adfe78
Roman <romansakhwork@gmail.com>
2022-12-17 16:44:48 +0300

    Added addCat function

--- app/src/main/java/com/example/catslist/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/catslist/viewmodels/MainActivityViewModel.kt
@@ -1,7 +1,14 @@
 package com.example.catslist.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.catslist.tools.CatStorage
 
 class MainActivityViewModel: ViewModel() {
+
+    fun addCat() {
+        Log.v("MainActivityViewModel","addCat")
+        CatStorage.getNewCatData()
+    }
 
 }
