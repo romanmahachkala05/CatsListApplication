7a450e5cad8e0c6b7382b085f904c97edd780314
Roman <romansakhwork@gmail.com>
2022-12-17 16:40:48 +0300

    Added function to load images with glide

--- app/src/main/java/com/example/catslist/viewmodels/CatsAdapter.kt
+++ app/src/main/java/com/example/catslist/viewmodels/CatsAdapter.kt
@@ -1,8 +1,13 @@
 package com.example.catslist.viewmodels
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.catslist.databinding.ItemCatBinding
 import com.example.catslist.models.Cat
 
@@ -37,4 +42,19 @@ class CatsAdapter : RecyclerView.Adapter<CatsAdapter.CatsViewHolder>() {
     class CatsViewHolder(
         val binding: ItemCatBinding
     ) : RecyclerView.ViewHolder(binding.root)
+
+}
+
+@BindingAdapter("catImage")
+fun setCatImage(view: ImageView, cat: Cat) {
+    view.setImageFromUrl(view.context, cat.url)
+}
+
+private fun ImageView.setImageFromUrl(context: Context, url: String) {
+    Glide.with(context)
+        .asBitmap()
+        .load(url)
+        .centerCrop()
+        .thumbnail()
+        .into(this)
 }
