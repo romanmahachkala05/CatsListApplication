cf89d0bfa4fb76708927ddbadf377db06b940acd
Roman <romansakhwork@gmail.com>
2022-12-17 16:43:01 +0300

    Added functions to get cat data, listeners

--- app/src/main/java/com/example/catslist/tools/CatStorage.kt
+++ app/src/main/java/com/example/catslist/tools/CatStorage.kt
@@ -1,13 +1,68 @@
 package com.example.catslist.tools
 
 import android.util.Log
-import androidx.lifecycle.MutableLiveData
 import com.example.catslist.models.Cat
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
+typealias CatsListener = (cats: List<Cat>) -> Unit
 
 object CatStorage {
 
+    private val tag = "CatStorage"
+    private var cats = mutableListOf<Cat>()
+    private val listeners = mutableListOf<CatsListener>()
+
+    fun getCats(): List<Cat> {
+        notifyChanges()
+        return cats
+    }
+
+    fun addListener(listener: CatsListener) {
+        listeners.add(listener)
+    }
+
+    fun removeListener(listener: CatsListener) {
+        listeners.remove(listener)
+    }
+
+    private fun notifyChanges() {
+        listeners.forEach { it.invoke(cats) }
+    }
+
+    fun getNewCatData() {
+        Log.v(tag, "getNewCat")
+        getCatData(object : DataReceivedCallback {
+            override fun onDataReceived(catData: Cat?) {
+                catData?.let {
+                    if (cats.contains(catData)) {
+                        Log.v(tag, "duplicate, adding new cat")
+                        getCatData(this)
+                    } else {
+                        Log.v(tag, "adding item $it to catsList")
+                        cats.add(it)
+                        getCats()
+                    }
+                }
+            }
+        })
+    }
+
+    fun getCatData(callback: DataReceivedCallback) {
+        Log.v(tag, "getCatData")
+        GetCatRetrofit.getService().requestCatInfo().enqueue(object :
+            Callback<List<Cat>> {
+            override fun onResponse(call: Call<List<Cat>>, response: Response<List<Cat>>) {
+                response.body()?.get(0)?.let {
+                    callback.onDataReceived(it)
+                }
+            }
+
+            override fun onFailure(call: Call<List<Cat>>, t: Throwable) {
+                TODO("Not yet implemented")
+            }
+        }
+        )
+    }
 }
