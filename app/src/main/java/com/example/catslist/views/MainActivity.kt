b114bd4e010d7c091657e42bd74424a33ba81788
Roman <romansakhwork@gmail.com>
2022-12-17 16:54:11 +0300

    Added adapter initialisation

--- app/src/main/java/com/example/catslist/views/MainActivity.kt
+++ app/src/main/java/com/example/catslist/views/MainActivity.kt
@@ -2,12 +2,34 @@ package com.example.catslist.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.catslist.R
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.catslist.App
+import com.example.catslist.databinding.ActivityMainBinding
+import com.example.catslist.tools.CatStorage
+import com.example.catslist.tools.CatsListener
+import com.example.catslist.viewmodels.CatsAdapter
+import com.example.catslist.viewmodels.MainActivityViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var adapter: CatsAdapter
+    private lateinit var binding: ActivityMainBinding
+
+    private val catsStorage: CatStorage
+        get() = (applicationContext as App).catsService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        adapter = CatsAdapter()
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerViewWithCats.layoutManager = layoutManager
+        binding.recyclerViewWithCats.adapter = adapter
+
+        CatStorage.addListener(catsListener)
+        repeat(5) { MainActivityViewModel().addCat() }
     }
 
     private val catsListener: CatsListener = {
