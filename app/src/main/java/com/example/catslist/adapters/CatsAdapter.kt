9935ef531af73f92b25df633aabba22fc686cc82
Roman <romansakhwork@gmail.com>
2022-12-18 16:47:06 +0300

    Removed binding adapters from CatsAdapter

--- app/src/main/java/com/example/catslist/adapters/CatsAdapter.kt
+++ app/src/main/java/com/example/catslist/adapters/CatsAdapter.kt
@@ -76,16 +76,4 @@ class CatsAdapter(
 
 }
 
-@BindingAdapter("catImage")
-fun setCatImage(view: ImageView, cat: Cat) {
-    view.setImageFromUrl(view.context, cat.url)
-}
 
-private fun ImageView.setImageFromUrl(context: Context, url: String) {
-    Glide.with(context)
-        .asBitmap()
-        .load(url)
-        .centerCrop()
-        .thumbnail()
-        .into(this)
-}
