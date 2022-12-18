e6462cf15526f7aa1fb7de481d13909c4ee8e5d8
Roman <romansakhwork@gmail.com>
2022-12-18 21:34:54 +0300

    Added insert and getAll functions

--- app/src/main/java/com/example/catslist/room/CatsDao.kt
+++ app/src/main/java/com/example/catslist/room/CatsDao.kt
@@ -1,8 +1,18 @@
 package com.example.catslist.room
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.catslist.models.Cat
 
 
-
+@Dao
 interface CatsDao {
+
+    @Insert
+    suspend fun insertCat(cat: Cat)
+
+    @Query("SELECT * FROM favoriteCats")
+    suspend fun getAll(): List<Cat>
 
 }
