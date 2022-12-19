4a4edbc36d18b528455a4a2bc0090104edae2ef0
Roman <romansakhwork@gmail.com>
2022-12-19 03:28:42 +0300

    Added viewPageAdapter

---
+++ app/src/main/java/com/example/catslist/adapters/ViewPageAdapter.kt
@@ -0,0 +1,16 @@
+package com.example.catslist.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class ViewPageAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        TODO("Not yet implemented")
+    }
+
+}
