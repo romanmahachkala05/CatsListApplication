14056db9db41c17f82548609d847a2eb6841161b
Roman <romansakhwork@gmail.com>
2022-12-19 10:02:35 +0300

    Added getInstance function

--- app/src/main/java/com/example/catslist/room/CatsDatabase.kt
+++ app/src/main/java/com/example/catslist/room/CatsDatabase.kt
@@ -1,9 +1,31 @@
 package com.example.catslist.room
 
+import android.content.Context
+import android.util.Log
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [CatDatabaseEntity::class], version = 1)
+@Database(entities = [CatDatabaseEntity::class], version = 2)
 abstract class CatsDatabase : RoomDatabase() {
     abstract fun catsDao(): CatsDao
+
+    companion object {
+        private var instance: CatsDatabase? = null
+
+        @Synchronized
+        fun getInstance(context: Context): CatsDatabase {
+            Log.v("starting building DB", "getInstance")
+            if (instance == null) {
+                Log.v("starting building DB", "puk")
+                instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    CatsDatabase::class.java, "cats_database"
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
+            }
+            return instance!!
+        }
+    }
 }
