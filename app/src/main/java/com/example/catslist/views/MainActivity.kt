e6763cc1b1c0d0abac61c0421fa437eba64d9f48
Roman <romansakhwork@gmail.com>
2022-12-19 10:11:57 +0300

    Removed unused code

--- app/src/main/java/com/example/catslist/views/MainActivity.kt
+++ app/src/main/java/com/example/catslist/views/MainActivity.kt
@@ -2,24 +2,9 @@ package com.example.catslist.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import androidx.lifecycle.lifecycleScope
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import androidx.room.Room
-import com.example.catslist.App
 import com.example.catslist.databinding.ActivityMainBinding
-import com.example.catslist.models.Cat
-import com.example.catslist.tools.CatStorage
-import com.example.catslist.tools.CatsListener
-import com.example.catslist.adapters.CatsActionsListener
-import com.example.catslist.adapters.CatsAdapter
 import com.example.catslist.adapters.ViewPageAdapter
-import com.example.catslist.room.*
-import com.example.catslist.viewmodels.MainActivityViewModel
 import com.google.android.material.tabs.TabLayoutMediator
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -34,24 +19,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        //testing room database
-        lateinit var catsDao: CatsDao
-        val catsDb = Room.databaseBuilder(
-            applicationContext,
-            CatsDatabase::class.java, "cats_database"
-        ).build()
-        catsDao = catsDb.catsDao()
-        fun testDB(){
-            lifecycleScope.launch(Dispatchers.IO) {
-                //catsDao.insertCat(CatDatabaseEntity("cat", "cat.com", 555, 555, true))
-                val allCats = catsDao.getAllCats()
-                Log.v(tag,"ALL CATS! $allCats")
-            }
-        }
-        testDB()
-
-        //ViewPage Adapter
         val viewPager = binding.viewPager
         val tabLayout = binding.tabLayout
         val viewPageAdapter = ViewPageAdapter(this, fragmentsList)
@@ -63,7 +30,6 @@ class MainActivity : AppCompatActivity() {
         TabLayoutMediator(tabLayout, viewPager){
             tab, pos -> tab.text = fragmentsNames[pos]
         }.attach()
-
 
     }
 }
