9576c63d29fa0b6a587d40ad15a23e9f9074fa90
Roman <romansakhwork@gmail.com>
2022-12-19 10:04:22 +0300

    Added logic for catListFragmentViewModel

--- app/src/main/java/com/example/catslist/viewmodels/CatsListFragmentViewModel.kt
+++ app/src/main/java/com/example/catslist/viewmodels/CatsListFragmentViewModel.kt
@@ -1,19 +1,60 @@
 package com.example.catslist.viewmodels
 
+import android.app.Application
 import android.app.DownloadManager
 import android.content.Context
 import android.net.Uri
 import android.os.Environment
 import android.util.Log
+import android.view.View
 import android.widget.Toast
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.widget.AppCompatImageButton
+import androidx.lifecycle.AndroidViewModel
+import com.example.catslist.R
+import com.example.catslist.models.Cat
+import com.example.catslist.room.CatDatabaseEntity
+import com.example.catslist.room.CatsDatabaseRepository
 import com.example.catslist.tools.CatStorage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.launch
 
-class CatsListFragmentViewModel : ViewModel() {
+class CatsListFragmentViewModel(app: Application) : AndroidViewModel(app) {
+
+
+    private val catsDatabaseRepository = CatsDatabaseRepository(app)
+    private val tag = "CatsListFragmentViewModel"
+
+    init {
+        Log.v(tag, "init")
+        CoroutineScope(Main).launch {
+            catsDatabaseRepository.getAllCats().forEach { CatStorage.favoriteCats.add(it.toCat()) }
+            Log.v(tag, "getAllCats, add each to CatStorage.favoriteCats = ${CatStorage.favoriteCats}")
+            CatStorage.notifyFavChanges()
+        }
+    }
 
     fun addCat() {
-        Log.v("MainActivityViewModel","addCat")
         CatStorage.getNewCatData()
+    }
+
+    fun onFavoriteButtonClick(cat: Cat, view: View){
+        CoroutineScope(Main).launch {
+            val icon = view.findViewById<AppCompatImageButton>(R.id.item_cat_star_button)
+            if (!(CatStorage.favoriteCats.any { it.id == cat.id})) {
+                cat.favorite = true
+                CatStorage.favoriteCats.add(cat)
+                CatStorage.notifyFavChanges()
+                catsDatabaseRepository.insert(CatDatabaseEntity.fromCat(cat))
+                icon.setBackgroundResource(R.drawable.ic_star_filled)
+            } else {
+                icon.setBackgroundResource(R.drawable.ic_star_empty)
+                CatStorage.favoriteCats.removeIf { it.id == cat.id }
+                CatStorage.notifyFavChanges()
+                catsDatabaseRepository.delete(CatDatabaseEntity.fromCat(cat))
+
+            }
+        }
     }
 
     fun downloadCatImage(context: Context, url: String, catId: String) {
