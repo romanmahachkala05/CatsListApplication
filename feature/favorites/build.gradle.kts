b8dbdda81d81c38c115f855852d104a0155f634b
user1234 <romansakhwork@gmail.com>
2026-09-21 03:36:46 +0300

    [TECH] Follow hiltViewModel to its new artifact and package

--- feature/favorites/build.gradle.kts
+++ feature/favorites/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.runtime.compose)
-    implementation(libs.androidx.hilt.navigation.compose)
+    implementation(libs.androidx.hilt.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation3.runtime)
     implementation(libs.kotlinx.collections.immutable)
     ksp(libs.androidx.hilt.compiler)
