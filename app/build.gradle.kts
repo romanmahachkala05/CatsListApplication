7b2d02e5c16865766ad641767416626c36a8538f
Roman <60145514+romanmahachkala05@users.noreply.github.com>
2026-09-21 19:40:13 +0300

    [TECH] Bump version to 2.2.1 (#75)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,7 +29,7 @@ val hasReleaseSigning = listOf("storeFile", "storePassword", "keyAlias", "keyPas
 // allowed 0-99 each.
 val versionMajor = 2
 val versionMinor = 2
-val versionPatch = 0
+val versionPatch = 1
 
 android {
     namespace = "com.example.catslist"
