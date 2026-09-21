65158b303e0def532fb84be9c11d7c63e458b678
user1234 <romansakhwork@gmail.com>
2026-09-21 03:02:24 +0300

    [TECH] Compile against SDK 37

--- build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidLibraryConventionPlugin.kt
@@ -17,7 +17,7 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
             }
 
             extensions.configure<LibraryExtension> {
-                compileSdk = 36
+                compileSdk = 37
 
                 defaultConfig {
                     minSdk = 27
